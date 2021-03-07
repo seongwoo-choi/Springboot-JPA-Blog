88c0d9f503236dd66a7a1ba13ca35480cd0a7fff
seongwoo-choi <how0326@naver.com>
2021-03-07 15:15:54 +0900

    글목록 보기

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -3,21 +3,36 @@ package com.cos.blog.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
+
+
+// 컨트롤러에서 세션에 접근하는 방법
+// @AuthenticationPrincipal PrincipalDetail principal
+// @AuthenticationPrincipal 어노테이션 => 컨트롤러에서 세션에 접근한다.
+// System.out.println("로그인 사용자 아이디 : "+principal.getUsername());
 
 @Controller
 public class BoardController {
 	
-	@GetMapping({"", "/"})
-	public String index() { // 컨트롤러에서 세션에 접근하는 방법
-		// @AuthenticationPrincipal PrincipalDetail principal
-		// @AuthenticationPrincipal 어노테이션 => 컨트롤러에서 세션에 접근한다.
-		// System.out.println("로그인 사용자 아이디 : "+principal.getUsername());
-		return "index";
-	}
+	@Autowired
+	private BoardService boardService;
 	
+	@GetMapping({"", "/"})
+	public String index(Model model) {
+		// main 페이지로 갈 때 데이터를 가져가야 함
+		// 데이터를 뷰로 가져가기 위해서 Model을 사용한다.
+		
+		model.addAttribute("boards", boardService.글목록());
+		// model의 이름이 boards 라는 이름으로 boardService.글목록()이 담겨져서 index.jsp로 데이터가 넘겨진다.
+		// jstl 태그라이브러리를 이용해서 ${boards}를 하면 데이터를 받을 수 있다.
+		
+		return "index"; // boards라는 모델이 index로 이동한다. 
+	}
+
 	// USER 권한이 필요
 	@GetMapping("/board/saveForm")
 	public String saveForm() {
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -2,6 +2,8 @@ package com.cos.blog.service;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +25,10 @@ public class BoardService {
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
+	}
+	
+	public List<Board> 글목록(){
+		return boardRepository.findAll();
 	}
 	
 	// boardService의 전체적인 흐름도
